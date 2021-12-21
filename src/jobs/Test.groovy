d06703621ade669feef35c0852deb4a75cf1748b
Lucas de los Santos <ldelossa@adobe.com>
2021-12-21 12:57:13 -0500

    adding more functionality to test job

--- src/jobs/Test.groovy
+++ src/jobs/Test.groovy
@@ -1,3 +1,23 @@
 pipelineJob('Jenkins Tutorial Demo - Simple DSL job') {
     description('A simple demo for Jenkins DSL')
+    parameters {
+        stringParam('Test', 'Test,', '')
+    }
+    definition {
+        cps{
+        sandbox()
+        script('''
+            pipeline {
+                agent any
+                stages {
+                    stage('Stage 1') {
+                        steps {
+                         echo 'logic'
+                        }
+                    }
+                }
+            }     
+        '''.stripIndent())
+        }
+    }
 }
