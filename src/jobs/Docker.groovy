4ee6441494ca22f5737148e6e6145bbd247e237b
Lucas de los Santos <anightonhoth@gmail.com>
2024-03-21 19:41:03 -0400

    Update Docker.groovy

--- src/jobs/Docker.groovy
+++ src/jobs/Docker.groovy
@@ -1,16 +1,27 @@
 pipelineJob('docker-test') {
-    agent {
-        docker {
-            image 'node:20.11.1-alpine3.19'
-            args '-v /var/run/docker.sock:/var/run/docker.sock'
-        }
-    }
-    stages {
-        stage('Test') {
-            steps {
-                sh 'npm --version'
-                sh 'node --version'
-            }
+    description('A simple demo for Jenkins DSL')
+    definition {
+        cps{
+        sandbox()
+        script('''
+            pipeline {
+                agent {
+                    docker {
+                        image 'node:20.11.1-alpine3.19'
+                        args '-v /var/run/docker.sock:/var/run/docker.sock'
+                    }
+                }
+                stages {
+                    stage('Test') {
+                        steps {
+                            sh 'npm --version'
+                            sh 'node --version'
+                        }
+                    }
+                }
+            }     
+        '''.stripIndent())
         }
     }
 }
+
