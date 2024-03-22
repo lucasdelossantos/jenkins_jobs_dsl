f277142bf1d8edf353f350f9af87d347eda7692c
Lucas de los Santos <anightonhoth@gmail.com>
2024-03-21 20:33:04 -0400

    Commenting out for the time being
    Will be worked on later

--- src/jobs/default_jobs_dsl.bak.groovy
+++ src/jobs/default_jobs_dsl.bak.groovy
@@ -3,3 +3,5 @@ import rareexportslab.dsl.factory.RareExportsLabTest
 
 def rareExportsLabTest = new RareExportsLabTest(this)
 rareExportsLabTest.testJob()
+
+// TODO - Implement the testJob method in the RareExportsLabTest class
